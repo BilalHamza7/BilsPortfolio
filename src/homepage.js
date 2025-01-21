0c05ff9cd9e04cadae4e73fd93a4e7fa14498582
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2025-01-21 12:35:53 +0530

    initial Commit

---
+++ src/homepage.js
@@ -0,0 +1,5 @@
+export default function Homepage(params) {
+    return (
+        <p>HomePage</p>
+    )
+};
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Homepage from './homepage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
